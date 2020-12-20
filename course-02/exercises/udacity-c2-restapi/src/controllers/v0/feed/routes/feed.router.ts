0dff102715f42e75400227ca08adb7b87e11d180
Christian Rölle <>
2020-12-20 03:00:27 +0100

    Added get single Feed by ID(PK)

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,7 +18,17 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id',
+    async (req: Request, res: Response) => {
+        let { id } = req.params;
 
+        if ( !id ) {
+            return res.status(400)
+                .send(`id is required`);
+        }
+        const item = await FeedItem.findByPk(id);
+        res.send(item)
+    });
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
